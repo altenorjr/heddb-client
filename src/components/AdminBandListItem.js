2095c6ce8de57b591132396a5da7695b96be29e3
Altenor Gonçalves <altenorjr@gmail.com>
2017-08-23 16:56:45 -0300

    .

--- src/components/AdminBandListItem.js
+++ src/components/AdminBandListItem.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import jss from 'react-jss';
 import { Map } from 'immutable';
-import cx from 'classnames';
-
-import LinesEllipsis from 'react-lines-ellipsis'
-import IconButton from 'material-ui/IconButton';
-import EditIcon from 'material-ui/svg-icons/editor/mode-edit';
-import DeleteIcon from 'material-ui/svg-icons/action/delete';
 
 import Band from './Band';
 
